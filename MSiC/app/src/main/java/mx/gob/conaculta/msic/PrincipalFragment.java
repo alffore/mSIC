e9deb760c44428dafd939100942e9d333f1ab888
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 15:22:49 -0600

    Se agregan 2 layers para la interfaz principal
    También se generalizan los permisos de acceso a Internet

--- MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
@@ -17,7 +17,7 @@ public class PrincipalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_principal, container, false);
         return rootView;
     }
 
