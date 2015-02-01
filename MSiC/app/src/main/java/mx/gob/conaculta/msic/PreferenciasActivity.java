8ffcef97ed639a2e14f05411cf3d1f7ce51d2453
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 10:46:55 -0600

    Se agrega una clase de Preferencias
    solo esqueleto

---
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/PreferenciasActivity.java
@@ -0,0 +1,20 @@
+package mx.gob.conaculta.msic;
+
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+
+/**
+ * Created by alfonso on 01/02/15.
+ */
+public class PreferenciasActivity  extends PreferenceActivity
+        implements Preference.OnPreferenceChangeListener{
+
+
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
