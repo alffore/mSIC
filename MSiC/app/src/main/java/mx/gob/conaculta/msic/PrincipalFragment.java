0f097809bfc717d229d23569afb300e016a525b8
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 11:25:00 -0600

    Se crea el esqueleto del fragmento Principal

---
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
@@ -0,0 +1,25 @@
+package mx.gob.conaculta.msic;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alfonso on 01/02/15.
+ */
+public class PrincipalFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+
+}
