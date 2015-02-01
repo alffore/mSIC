6b520d9eea6cd813512d95a264826ea5e52491f6
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 11:40:11 -0600

    Cambio de versión minima

--- MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
@@ -1,8 +1,8 @@
 package mx.gob.conaculta.msic;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
