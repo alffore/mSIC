30d26d9d45065295326f812e029384b839517116
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 19:53:42 -0600

    Version demo que checa el certificado y correcto
    para GMaps

--- MSiC/app/src/main/java/mx/gob/conaculta/msic/MainActivity.java
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/MainActivity.java
@@ -17,12 +17,13 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.demo_map);
+       /* setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
 
