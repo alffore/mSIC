4a0da23cf28966f8c365b794abb46fb7abb6d932
A. Alfonso F. R <alffore@gmail.com>
2015-02-01 21:41:23 -0600

    Primer versión de GridView funcionando

---
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/MenuTemasAdapter.java
@@ -0,0 +1,47 @@
+package mx.gob.conaculta.msic;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by alfonso on 01/02/15.
+ */
+public class MenuTemasAdapter extends ArrayAdapter{
+
+Context context;
+
+    private static final String[] MT_ARRAY={"Museo","Teatro","Casas y Centros Culturales","Librerías","Galerías","Bibliotecas"};
+
+
+    public MenuTemasAdapter(Context context){
+        super(context,0);
+        this.context=context;
+    }
+
+    public int getCount(){
+        return MT_ARRAY.length;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row = convertView;
+
+        if(row == null){
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(R.layout.grid_celda, parent, false);
+
+            TextView tv =(TextView) row.findViewById(R.id.textViewGM);
+
+            tv.setText(MT_ARRAY[position]);
+
+        }
+
+        return row;
+    }
+
+}
--- MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/PrincipalFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by alfonso on 01/02/15.
@@ -21,6 +24,18 @@ public class PrincipalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_principal, container, false);
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridView);
+
+        gridview.setAdapter(new MenuTemasAdapter(getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         return rootView;
     }
 
