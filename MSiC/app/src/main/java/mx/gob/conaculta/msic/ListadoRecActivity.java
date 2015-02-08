b0762473a6de303ded64e9c44b435828f07c9ef3
A. Alfonso F. R <alffore@gmail.com>
2015-02-08 16:56:50 -0600

    Se agrega el esqueleto de un Listado

---
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/ListadoRecActivity.java
@@ -0,0 +1,16 @@
+package mx.gob.conaculta.msic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alfonso on 08/02/15.
+ */
+public class ListadoRecActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
