d0f32f55cb5c9a24502453c4c4dec339e897648e
A. Alfonso F. R <alffore@gmail.com>
2015-03-16 21:03:06 -0600

    Se desarrollan los métodos para el calculo de distancias

--- MSiC/app/src/main/java/mx/gob/conaculta/msic/utils/Utiles.java
+++ MSiC/app/src/main/java/mx/gob/conaculta/msic/utils/Utiles.java
@@ -29,26 +29,11 @@ public class Utiles {
      */
     public static double distRecPunto(Recurso rec, double lat, double lon) {
 
-        double daux = Math.sin(rec.lat) * Math.sin(lat);
+        double lat0=rec.lat*MSiCConst.D2R;
+        double lon0=rec.lon*=MSiCConst.D2R;
+        lat*=MSiCConst.D2R;
+        lon*=MSiCConst.D2R;
 
-        daux += Math.cos(rec.lat) * Math.cos(rec.lon) * Math.cos(lat) * Math.cos(lon);
-
-        daux += Math.cos(rec.lat) * Math.sin(rec.lon) * Math.cos(lat) * Math.sin(lon);
-
-        daux = MSiCConst.RT * Math.acos(daux);
-
-        return daux;
-    }
-
-    /**
-     *
-     * @param lat0
-     * @param lon0
-     * @param lat
-     * @param lon
-     * @return
-     */
-    public static double distPP(double lat0, double lon0, double lat, double lon) {
         double daux = Math.sin(lat0) * Math.sin(lat);
 
         daux += Math.cos(lat0) * Math.cos(lon0) * Math.cos(lat) * Math.cos(lon);
@@ -61,18 +46,46 @@ public class Utiles {
     }
 
     /**
-     *
+     * Metodo para calcular la distancia entre puntos en metros
+     * @param lat0
+     * @param lon0
+     * @param lat
+     * @param lon
+     * @return
+     */
+    public static double distPP(double lat0, double lon0, double lat, double lon) {
+
+        lat0*=MSiCConst.D2R;
+        lon0*=MSiCConst.D2R;
+        lat*=MSiCConst.D2R;
+        lon*=MSiCConst.D2R;
+
+        double daux = Math.sin(lat0) * Math.sin(lat);
+
+        daux += Math.cos(lat0) * Math.cos(lon0) * Math.cos(lat) * Math.cos(lon);
+
+        daux += Math.cos(lat0) * Math.sin(lon0) * Math.cos(lat) * Math.sin(lon);
+
+        daux = MSiCConst.RT * Math.acos(daux);
+
+        return daux;
+    }
+
+    /**
+     * Metodo para formatear la salidad de distancias
      * @param dist
      * @return
      */
     public static String salidaDist(double dist) {
         String sal = "";
 
-        if (dist >= 1000) {
-            DecimalFormat formatter = new DecimalFormat("#,###.##");
-            sal = "(" + formatter.format(dist / 1000) + "km)";
+        DecimalFormat formatter = new DecimalFormat("#,###.##");
+
+        if (dist >= 1000.00) {
+
+            sal = " (" + formatter.format(dist / 1000.00) + "km)";
         } else {
-            sal = "(" + String.valueOf(dist) + "m)";
+            sal = " (" + formatter.format(dist) + "m)";
         }
 
         return sal;
